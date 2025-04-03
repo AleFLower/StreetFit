d291b213aea0442811b70b73fccb468a02edd59e
AleFior <107346301+AleFLower@users.noreply.github.com>
2025-04-03 18:43:13 +0200

    Update MainCLI.java

--- StreetFit/src/com/streetfit/main/MainCLI.java
+++ StreetFit/src/com/streetfit/main/MainCLI.java
@@ -5,7 +5,7 @@ import com.streetfit.controllercli.HomeGUIControllerCLI;
 public class MainCLI {
 
 	public static void run() {
-		// TODO Auto-generated method stub
+		
 		HomeGUIControllerCLI controller = new HomeGUIControllerCLI();
 		controller.start();
 		
