14833c107b39a8b3c84f671446d70c0813403c1c
AleFior <107346301+AleFLower@users.noreply.github.com>
2025-04-03 18:09:27 +0200

    Update HomeGUIControllerCLI.java

--- StreetFit/src/com/streetfit/controllercli/HomeGUIControllerCLI.java
+++ StreetFit/src/com/streetfit/controllercli/HomeGUIControllerCLI.java
@@ -4,14 +4,12 @@ import com.streetfit.model.Credentials;
 import com.streetfit.controller.Controller;
 
 public class HomeGUIControllerCLI implements Controller{
-
-	private Credentials cred;
 	
 	public void start() {
 		//i have to retrieve login information from loginController		
 		LoginControllerCLI controller = new LoginControllerCLI();
 		controller.start();
-		cred = controller.getCred();   
+		Credentials cred = controller.getCred();   
 		
 		
 		if(cred.getRole() == null) {
