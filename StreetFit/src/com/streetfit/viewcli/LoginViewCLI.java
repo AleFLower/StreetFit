9c4e700b4596a9d1aca991f74c249e3df65cd8e5
AleFior <107346301+AleFLower@users.noreply.github.com>
2025-04-07 18:35:15 +0200

    Update LoginViewCLI.java

--- StreetFit/src/com/streetfit/viewcli/LoginViewCLI.java
+++ StreetFit/src/com/streetfit/viewcli/LoginViewCLI.java
@@ -31,13 +31,23 @@ public class LoginViewCLI {
     }
 
     public static CredentialsBean authenticate() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("username: ");
-        String username = reader.readLine();
-        System.out.print("password: ");
-        String password = reader.readLine();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        return new CredentialsBean(username, password, null);
+    while (true) {
+        try {
+            System.out.print("username: ");
+            String username = reader.readLine();
+
+            System.out.print("password: ");
+            String password = reader.readLine();
+
+            return new CredentialsBean(username, password, null);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Please try again.\n");
+        }
     }
+}
+
 }
 
