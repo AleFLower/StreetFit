96b2732ba3052fa3046ff4d717c3b5ab00635182
AleFior <107346301+AleFLower@users.noreply.github.com>
2025-05-24 11:03:05 +0200

    Update NotificationQueue.java

--- StreetFit/src/com/streetfit/utils/NotificationQueue.java
+++ StreetFit/src/com/streetfit/utils/NotificationQueue.java
@@ -6,34 +6,27 @@ import java.util.Queue;
 import com.streetfit.model.TrainerNotification;
 
 public class NotificationQueue {
+    private static NotificationQueue instance;
     private final Queue<TrainerNotification> queue = new LinkedList<>();
 
     private NotificationQueue() {}
 
-    // Holder class: thread-safe, lazy loading garantito dalla JVM
-    private static class Holder {
-        private static final NotificationQueue INSTANCE = new NotificationQueue();
+    public static synchronized NotificationQueue getInstance() {
+        if (instance == null) {
+            instance = new NotificationQueue();
+        }
+        return instance;
     }
 
-    public static NotificationQueue getInstance() {
-        return Holder.INSTANCE;
-    }
-
-    public synchronized void addNotification(TrainerNotification n) {
+    public void addNotification(TrainerNotification n) {
         queue.add(n);
     }
 
-    public synchronized Queue<TrainerNotification> getAndClearNotifications() {
+    public Queue<TrainerNotification> getAndClearNotifications() {
         Queue<TrainerNotification> copy = new LinkedList<>(queue);
         queue.clear();
         return copy;
     }
-
-    public synchronized boolean hasNotifications() {
-        return !queue.isEmpty();
-    }
-}
-
 
     public boolean hasNotifications() {
         return !queue.isEmpty();
