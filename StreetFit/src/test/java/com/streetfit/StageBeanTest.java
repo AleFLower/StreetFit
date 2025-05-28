f0d80e266e6568818948d136c7f375ac1a30126a
AleFior <107346301+AleFLower@users.noreply.github.com>
2025-05-28 19:35:14 +0200

    Update StageBeanTest.java

--- StreetFit/src/test/java/com/streetfit/StageBeanTest.java
+++ StreetFit/src/test/java/com/streetfit/StageBeanTest.java
@@ -10,18 +10,18 @@ import main.java.com.streetfit.beans.StageBean;
 
 
 
-public class StageBeanTest {
+ class StageBeanTest {
 
     // Test per il metodo isValid() in caso di categoria non valida
     @Test
-    public void testInvalidCategory() {
+     void testInvalidCategory() {
         StageBean stage = new StageBean("Stage 1", "Itinerary", "InvalidCategory", new Date(), "Place", 20);
         assertFalse(stage.isValid(), "Category should be valid (Functional, Yoga, HIIT, Stretching, Pilates, Crossfit)");
     }
 
     // Test per il metodo isValid() in caso di max partecipanti non valido (<= 0)
     @Test
-    public void testInvalidMaxParticipants() {
+  void testInvalidMaxParticipants() {
         StageBean stage = new StageBean("Stage 1", "Itinerary", "Yoga", new Date(), "Place", 0);
         assertFalse(stage.isValid(), "Max participants should be greater than 0");
     }
