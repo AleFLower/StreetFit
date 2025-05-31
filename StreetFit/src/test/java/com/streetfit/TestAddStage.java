79f888cabd058a90f321987e9a009efe5ed75c39
AleFior <107346301+AleFLower@users.noreply.github.com>
2025-05-31 14:01:57 +0200

    Update TestAddStage.java

--- StreetFit/src/test/java/com/streetfit/TestAddStage.java
+++ StreetFit/src/test/java/com/streetfit/TestAddStage.java
@@ -1,6 +1,6 @@
 package test.java.com.streetfit;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;  // Cambia da org.junit.Assert a org.junit.jupiter.api.Assertions
 
 import java.sql.Date;
 
@@ -11,22 +11,22 @@ import main.java.com.streetfit.controller.AddStageController;
 import main.java.com.streetfit.model.TrainingStage;
 
 class TestAddStage {
-	
-	@Test
-	// test che verifica se viene aggiunta una nuova stage, allora la lista di stage ha dimensione 1
-	void testCreateStage() {
-		int dim = 0;
-		StageBean bean = new StageBean("Streetworkout RM", "circuit PHA", "functional",Date.valueOf("2025-10-10"),"Rome", 20 );
-		AddStageController controller = new AddStageController();
-		
-		try {
-		controller.addstage(new TrainingStage(bean.getTitle(),bean.getItinerary(),bean.getCategory(),bean.getDate(),bean.getPlace(), bean.getMaxParticipants()));
-		dim = controller.getAllStages().size();
-		}catch(Exception e) {
-			dim = -1;
-		}
-		
-		assertEquals(1,dim);
-		
-	}
+
+    @Test
+    // test che verifica se viene aggiunta una nuova stage, allora la lista di stage ha dimensione 1
+    void testCreateStage() {
+        int dim = 0;
+        StageBean bean = new StageBean("Streetworkout RM", "circuit PHA", "functional",Date.valueOf("2025-10-10"),"Rome", 20 );
+        AddStageController controller = new AddStageController();
+
+        try {
+            controller.addstage(new TrainingStage(bean.getTitle(), bean.getItinerary(), bean.getCategory(), bean.getDate(), bean.getPlace(), bean.getMaxParticipants()));
+            dim = controller.getAllStages().size();
+        } catch(Exception e) {
+            dim = -1;
+        }
+
+        assertEquals(1, dim);  // Usa assertEquals di JUnit 5
+    }
 }
+
